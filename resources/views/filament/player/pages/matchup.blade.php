ecdcc03e627c7e1e872a2474ecc9dc5c47a5c5a4
Brad Goldsmith <bradgoldsmith@Brads-MacBook-Air.local>
2025-08-17 11:06:39 -0400

    edned up with 2 sperate divs for the mobile / tablet beacuse honestly i'm not sure what else would've worked.  I kept getting little annoyances that were not quite doing what I needed.

--- resources/views/filament/player/pages/matchup.blade.php
+++ resources/views/filament/player/pages/matchup.blade.php
@@ -16,7 +16,7 @@
 
             <div class="-mx-6 border-t border-gray-200 dark:border-white/3"></div>
 
-            <div class="">
+            <div class="md:hidden">
                 <div class="py-8 space-y-6">
                     <div class="flex items-center justify-between">
                         <div class="flex flex-col">
@@ -75,6 +75,71 @@
                     </div>
                 </div>
             </div>
+
+            <div class="hidden md:flex flex-row justify-between items-center">
+
+                <div class="py-8 space-y-6 w-full md:w-1/2 md:px-6">
+                    <div class="flex items-center justify-between">
+                        <div class="flex flex-col">
+                            <span class="text-base font-semibold">Player 1</span>
+                            <span class="text-base font-semibold">Player 2</span>
+                        </div>
+                        <div
+                            class="size-12 rounded-full bg-gray-100 dark:bg-white/10 grid place-items-center text-xs text-gray-500">
+                            LOGO
+                        </div>
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <button type="button"
+                            class="size-12 px-2 rounded-full border border-gray-200 dark:border-white/3 grid place-items-center">
+                            <span class="text-2xl leading-none">−</span>
+                        </button>
+                        <div
+                            class="min-w-[88px] px-6 h-12 rounded grid place-items-center border border-gray-200 dark:border-white/3">
+                            <span class="text-2xl font-bold">3</span>
+                        </div>
+                        <button type="button"
+                            class="size-12 px-2 rounded-full border border-gray-200 dark:border-white/3 grid place-items-center">
+                            <span class="text-2xl leading-none">+</span>
+                        </button>
+                    </div>
+                </div>
+
+
+                <div class="border-t border-gray-200 dark:border-white/3 w-full md:hidden"></div>
+                <div class="hidden md:block w-px bg-gray-200 dark:bg-white/3 self-stretch"></div>
+
+
+                <div class="py-8 space-y-6 w-full md:w-1/2 md:px-6">
+                    <div class="flex items-center justify-between">
+                        <div class="flex flex-col">
+                            <span class="text-base font-semibold">Player 3</span>
+                            <span class="text-base font-semibold">Player 4</span>
+                        </div>
+                        <div
+                            class="size-12 rounded-full bg-gray-100 dark:bg-white/10 grid place-items-center text-xs text-gray-500">
+                            LOGO
+                        </div>
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <button type="button"
+                            class="size-12 px-2 rounded-full border border-gray-200 dark:border-white/3 grid place-items-center">
+                            <span class="text-2xl leading-none">−</span>
+                        </button>
+                        <div
+                            class="min-w-[88px] px-6 h-12 rounded grid place-items-center border border-gray-200 dark:border-white/3">
+                            <span class="text-2xl font-bold">3</span>
+                        </div>
+                        <button type="button"
+                            class="size-12 px-2 rounded-full border border-gray-200 dark:border-white/3 grid place-items-center">
+                            <span class="text-2xl leading-none">+</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
 
             <div class="-mx-6 border-t border-gray-200 dark:border-white/3"></div>
 
